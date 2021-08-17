532735f93e7d1159a4e67512ed7929a5ec87f260
Esmaeil Chitgar <84287711+EsmaeilChitgar@users.noreply.github.com>
2021-08-17 18:33:28 +0430

    Adding the project source to repository.
    Adding the project source to repository.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AxAXVLC;
+
+namespace VlcForRtspWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly AxVLCPlugin2 vlc;
+
+        [Display]
+        public int MyProperty { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            string s = null;
+
+            var r = s?.ToString() ?? string.Empty;
+            return;
+
+
+            vlc = new AxVLCPlugin2();
+            windowsFormsHost.Child = vlc;
+
+
+
+            int i = 1;
+            while (i == 0b1)
+            {
+                try
+                {
+                    Debug.WriteLine("try");
+                    //return;
+                    Application.Current.Shutdown();
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    Debug.WriteLine("finalyl");
+                }
+
+                Debug.WriteLine("end of while.");
+            }
+
+            Debug.WriteLine("at the end.");
+        }
+
+        private void btnPlayLocalFile_Click(object sender, RoutedEventArgs e)
+        {
+            String localFile = @"file:///C:\Users\e.chitgar\Desktop\12-25-27.mp4";
+            int id = vlc.playlist.add(localFile);
+            vlc.playlist.playItem(id);
+        }
+
+        private void btnPlayRtspStream_Click(object sender, RoutedEventArgs e)
+        {
+            string rtspUrl = "rtsp://root:root@192.168.1.114/axis-media/media.amp?videocodec=h264&resolution=800x450&fps=8&compression=50";
+            int id = vlc.playlist.add(rtspUrl);
+            vlc.playlist.playItem(id);
+        }
+    }
+}
---
+++ VLC.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace VlcForRtspWpf
+{
+    partial class VLC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VLC));
+            this.axVLCPlugin210 = new AxAXVLC.AxVLCPlugin2();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin210)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin210
+            // 
+            this.axVLCPlugin210.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axVLCPlugin210.Enabled = true;
+            this.axVLCPlugin210.Location = new System.Drawing.Point(0, 0);
+            this.axVLCPlugin210.Name = "axVLCPlugin210";
+            this.axVLCPlugin210.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin210.OcxState")));
+            this.axVLCPlugin210.Size = new System.Drawing.Size(483, 396);
+            this.axVLCPlugin210.TabIndex = 0;
+            // 
+            // VLC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axVLCPlugin210);
+            this.Name = "VLC";
+            this.Size = new System.Drawing.Size(483, 396);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin210)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin210;
+    }
+}
